0fd990ade5fb2ff3949c6b158214f097df44eb4d
Tommy Ngx <tommyfederation@gmail.com>
2023-12-18 10:15:13 +1100

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,7 +1,7 @@
 
 import segmentation_models_pytorch as smp
 
-class DiceLoss.smp.utils.losses.DiceLoss):
+class DiceLoss(smp.utils.losses.DiceLoss):
     def __init__(self):
         super(DiceLoss, self).__init__()
 
