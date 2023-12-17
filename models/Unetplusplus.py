1f860a0bd29909438a2afc66792a7128a4c2d966
Tommy Ngx <tommyfederation@gmail.com>
2023-12-18 09:22:20 +1100

    Update Unetplusplus.py

--- models/Unetplusplus.py
+++ models/Unetplusplus.py
@@ -1,10 +1,18 @@
 import torch.nn as nn
+import segmentation_models_pytorch as smp
 
-class Unet(nn.Module):
+class UnetPlusPlus(nn.Module):
     def __init__(self, encoder_name, encoder_weights, in_channels, classes, activation):
-        super(Unet, self).__init__()
-        # Your model architecture implementation here
+        super(UnetPlusPlus, self).__init__()
+
+        # Use the provided encoder and other parameters
+        self.model = smp.UnetPlusPlus(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            in_channels=in_channels,
+            classes=classes,
+            activation=activation
+        )
 
     def forward(self, x):
-        # Forward pass implementation
-        return x
+        return self.model(x)
