6b95c281ff27a6d3ab4711f028656b8b11659461
Tommy Ngx <tommyfederation@gmail.com>
2023-12-18 10:14:16 +1100

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,23 +1,8 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
-class DiceLoss(nn.Module):
-    def __init__(self, smooth=1.):
+import segmentation_models_pytorch as smp
+
+class DiceLosssmp.utils.losses.DiceLoss):
+    def __init__(self):
         super(DiceLoss, self).__init__()
-        self.smooth = smooth
 
-    def forward(self, logits, targets):
-        num_classes = logits.size(1)
-        true_1_hot = F.one_hot(targets, num_classes).float()
-
-        probs = F.softmax(logits, dim=1)
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).contiguous()
-        probs = probs.permute(0, 2, 3, 1).contiguous()
-
-        intersection = torch.sum(probs * true_1_hot, dim=(0, 1, 2))
-        cardinality = torch.sum(probs + true_1_hot, dim=(0, 1, 2))
-
-        dice_loss = (2. * intersection + self.smooth) / (cardinality + self.smooth)
-
-        return 1 - dice_loss.mean()
+    # You can add any additional customization or logic here if needed
