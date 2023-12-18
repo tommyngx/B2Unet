ebbd1ca274676f1d495ab88a1022653361e9d2b8
Tommy Ngx <tommyfederation@gmail.com>
2023-12-18 11:08:04 +1100

    Update test.py

--- test.py
+++ test.py
@@ -82,4 +82,4 @@ if __name__ == "__main__":
     parser.add_argument("--model_path", type=str, required=True, help="Path to the folder where models are saved")
     args = parser.parse_args()
 
-    main(args.config, args.model_save_link)
+    main(args.config, args.model_path)
