4342780b168773ee96a17ced00c15baf98b8c396
Tommy Ngx <tommyfederation@gmail.com>
2023-12-18 11:02:44 +1100

    Update test.py

--- test.py
+++ test.py
@@ -1,44 +1,85 @@
 import torch
 from torch.utils.data import DataLoader
-from dataset import Dataset
-from models import UnetPlusPlus  # Update with your actual module and class names
-from utils import get_validation_augmentation, get_preprocessing  # Update with your actual module names
-from loss import DiceLoss  # Update with your actual loss class from loss.py
-from configs.config import Config
+import segmentation_models_pytorch as smp
+from dataset import Dataset  # Import your Dataset class
+from loss import DiceLossWrapper  # Import your custom loss or use smp.utils.losses.DiceLoss
+from configs import Config  # Import your Config class
+from utils import visualize  # Import your visualization function
 
-def main():
-    # Load configurations
-    config_module = __import__("config_unetplusplus")  # Update with the correct configuration file
+def main(config_file, model_save_link):
+    # Load config
+    config_module = __import__(config_file)
     model_config = config_module.Config()
 
+    # Load model
+    best_model = torch.load(f'{model_save_link}')
+    best_model.to(model_config.DEVICE)
+
     # Load test dataset
-    test_dataset = Dataset(config=model_config, split='test', classes=model_config.CLASSES, augmentation=get_validation_augmentation(), preprocessing=get_preprocessing(preprocessing_fn))
-    test_loader = DataLoader(test_dataset, batch_size=model_config.BATCH_SIZE, shuffle=False, num_workers=2)
-
-    # Define the model, loss, and metrics
-    model = UnetPlusPlus()  # Update with your actual model class
-    model.load_state_dict(torch.load(model_config.MODEL_SAVE_PATH))  # Load the trained model weights
-    model.eval()
-
-    loss = DiceLoss()  # Update with your actual loss class from loss.py
-    metrics = model_config.METRICS_TEST  # Update with your actual test metrics
-
-    # Define test epoch
-    test_epoch = smp.utils.train.ValidEpoch(
-        model, 
-        loss=loss, 
-        metrics=metrics,
-        device=model_config.DEVICE,
-        verbose=True,
+    test_dataset = Dataset(
+        images_dir=model_config.IMAGES_DIR,
+        masks_dir=model_config.MASKS_DIR,
+        csv_path=model_config.TEST_CSV_PATH,
+        split="test",
+        classes=model_config.CLASSES,
+        augmentation=model_config.get_validation_augmentation(),
+        preprocessing=model_config.get_preprocessing(model_config.get_preprocessing_fn())
     )
 
-    # Run the test epoch
-    test_logs = test_epoch.run(test_loader)
+    test_dataloader = DataLoader(test_dataset)
 
-    # Print or save the test results
-    print("Test Results:")
-    for key, value in test_logs.items():
-        print(f"{key}: {value}")
+    # Define metrics for evaluation
+    metrics_all = [
+        smp.utils.metrics.IoU(threshold=0.5),
+        smp.utils.metrics.Fscore(threshold=0.5),
+        smp.utils.metrics.Accuracy(threshold=0.5),
+    ]
+
+    # Create test epoch
+    test_epoch = smp.utils.train.ValidEpoch(
+        model=best_model,
+        loss=DiceLossWrapper(),  # Use your custom loss or smp.utils.losses.DiceLoss
+        metrics=metrics_all,
+        device=model_config.DEVICE,
+    )
+
+    test_dataset_vis = Dataset(
+            images_dir=model_config.IMAGES_DIR,
+            masks_dir=model_config.MASKS_DIR,
+            csv_path=model_config.TEST_CSV_PATH,
+            split="test",
+            classes=model_config.CLASSES,
+            augmentation=model_config.get_validation_augmentation(),
+)
+
+    # Run evaluation on the test set
+    logs = test_epoch.run(test_dataloader)
+
+    # Visualize results
+    for i in range(5):
+        n = np.random.choice(len(test_dataset))
+
+        image_vis = test_dataset[n][0].astype('uint8')
+        image, gt_mask = test_dataset[n]
+
+        gt_mask = gt_mask.squeeze()
+
+        x_tensor = torch.from_numpy(image).to(model_config.DEVICE).unsqueeze(0)
+        pr_mask = best_model.predict(x_tensor)
+        pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+
+        visualize(
+            image=image_vis,
+            ground_truth_mask=gt_mask,
+            predicted_mask=pr_mask
+        )
 
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Test the segmentation model on the test set.")
+    parser.add_argument("--config", type=str, required=True, help="Path to the config file")
+    parser.add_argument("--model_save_link", type=str, required=True, help="Path to the folder where models are saved")
+    args = parser.parse_args()
+
+    main(args.config, args.model_save_link)
